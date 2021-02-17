dac51302de93c245da15785aa18ec7d510a7f963
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-02-17 23:04:32 +0800

    add lark-upload-image

---
+++ cmd/lark-upload-image/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/caiguanhao/lark-slim"
+)
+
+func die(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a...)
+	os.Exit(1)
+}
+
+func main() {
+	var appId, appSecret, imageType, sendTarget string
+	flag.StringVar(&appId, "app-id", "", "lark app id (you can also use env LARK_APP_ID)")
+	flag.StringVar(&appSecret, "app-secret", "", "lark app secret (you can also use env LARK_APP_SECRET)")
+	flag.StringVar(&imageType, "type", "message", "image type (message or avatar)")
+	flag.StringVar(&sendTarget, "send", "", "also send image message to open_id, user_id, email or chat_id")
+	flag.Usage = func() {
+		fmt.Fprintf(
+			flag.CommandLine.Output(),
+			"Usage: %s [file ...] - %s\n", os.Args[0],
+			"upload images from files or stdin to lark",
+		)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if appId == "" {
+		appId = os.Getenv("LARK_APP_ID")
+	}
+	if appId == "" {
+		die("error: empty app id")
+	}
+
+	if appSecret == "" {
+		appSecret = os.Getenv("LARK_APP_SECRET")
+	}
+	if appSecret == "" {
+		die("error: empty app secret")
+	}
+
+	l := lark.API{
+		AppId:     appId,
+		AppSecret: appSecret,
+	}
+	_, err := l.GetAccessToken()
+	if err != nil {
+		die(err)
+	}
+
+	args := flag.Args()
+	var uploadFunc func(io.Reader) (string, error)
+	if imageType == "message" {
+		uploadFunc = l.UploadMessageImage
+	} else if imageType == "avatar" {
+		uploadFunc = l.UploadAvatarImage
+	} else {
+		die("unknown image type")
+	}
+
+	var hasErrors bool
+
+	process := func(key string) {
+		if sendTarget != "" {
+			err := l.SendImageMessage(sendTarget, key)
+			if err != nil {
+				hasErrors = true
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		fmt.Println(key)
+	}
+
+	if len(args) == 0 {
+		key, err := uploadFunc(os.Stdin)
+		if err != nil {
+			die(err)
+		}
+		process(key)
+		return
+	}
+
+	for _, fn := range args {
+		f, err := os.Open(fn)
+		if err != nil {
+			hasErrors = true
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		key, err := uploadFunc(f)
+		f.Close()
+		if err != nil {
+			hasErrors = true
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		process(key)
+	}
+	if hasErrors {
+		os.Exit(1)
+	}
+}
